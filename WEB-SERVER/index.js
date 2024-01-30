7a738179fe7752707444ed8a4c6f2640db20df8a
ambar gupta <ambar.gtm@gmail.com>
2024-01-30 23:07:17 +0530

    ignore /favicon.ico request

--- WEB-SERVER/index.js
+++ WEB-SERVER/index.js
@@ -3,6 +3,9 @@ const fs = require("fs");
 
 
 const server = http.createServer((req,res) =>{  
+    // ignore the /favicon.ico request 
+    if(req.url==='/favicon.ico')
+        return res.end();
     // create a log pettron  when any new request comes it shoud be log in log file  
       const log = `${Date.now()}: url: ${req.url}: request has recieved \n`;
       // now call fs filesystem  for  create a log file and append the log in each time
@@ -22,7 +25,6 @@ const server = http.createServer((req,res) =>{
       
 
 });
-
 server.listen(8000,()=>{
     console.log("--------------->web server has  started<------------------");
 });
